9b6371ee17a25d322aa769c16429861c9557bace
Andreas-Keller <andreaskeller2010@gmail.com>
2013-12-19 17:02:15 +0100

    last commit thursday 19

--- Soundplayer/src/ch/brownbag/soundplayer/MainActivity.java
+++ Soundplayer/src/ch/brownbag/soundplayer/MainActivity.java
@@ -1,18 +1,29 @@
 package ch.brownbag.soundplayer;
 
 import android.app.*;
+import android.content.Intent;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
 
 public class MainActivity extends Activity
-{
+{	
+	 public final static String EXTRA_MESSAGE = "ch.brownbag.soundplayer.MESSAGE";
     /** Called when the activity is first created.  */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view)
+    {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
     }
 }
 
