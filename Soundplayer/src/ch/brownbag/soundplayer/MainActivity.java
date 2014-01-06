70ada0ba835fa43edefabf27b8aa6869967a5e50
Andreas-Keller <andreaskeller2010@gmail.com>
2014-01-06 12:07:30 +0100

    commit 1200 6.1.14

--- Soundplayer/src/ch/brownbag/soundplayer/MainActivity.java
+++ Soundplayer/src/ch/brownbag/soundplayer/MainActivity.java
@@ -1,24 +1,49 @@
 package ch.brownbag.soundplayer;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends Activity
+public class MainActivity extends Activity implements OnClickListener
 {	
-	 public final static String EXTRA_MESSAGE = "ch.brownbag.soundplayer.MESSAGE";
+	public final static String EXTRA_MESSAGE = "ch.brownbag.soundplayer.MESSAGE";
+    
+	Button playButton;
+		
     /** Called when the activity is first created.  */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+        playButton=(Button)this.findViewById(R.id.Button01);
+        playButton.setOnClickListener(this);
+	}
+    @Override
+   	public void onClick(View v) {
+   		Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+   		
+   		File sdcard = Environment.getExternalStorageDirectory();
+   		
+   		File audioFile = new File(sdcard.getPath() + "music/PUFF_FINAL.mp3");
+   		
+   		intent.setDataAndType(Uri.fromFile(audioFile), "audio/mp3");
+   		startActivity(intent);   		
+   		
+   	}
+    
+        
     /** Called when the user clicks the Send button */
     public void sendMessage(View view)
     {
@@ -49,7 +74,8 @@ public class MainActivity extends Activity
                 return super.onOptionsItemSelected(item);
         }
     }
-}
+	
+}//end class MainActivity
 
 // this is just a comment
 // this is another comment from thursday 19 dec 16:00
